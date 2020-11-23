1e2610122ef72b8a01357833d040be2291808a7b
LeBattant <LeBattant@hotmail.com>
2020-11-23 08:02:12 -0500

    initial commit for project structure

---
+++ src/main/java/com/example/stockmarketdowjones/StockmarketdowjonesApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketdowjones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmarketdowjonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmarketdowjonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stockmarketdowjones/StockmarketdowjonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketdowjones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmarketdowjonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
