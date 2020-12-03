6781720d195690788d5acd3c3ca3a64e24355adf
LeBattant <LeBattant@hotmail.com>
2020-12-02 21:13:10 -0500

    simplify exception handling

--- src/main/java/com/example/stockmarketdowjones/exception/DowJonesDataControllerAdvice.java
+++ src/main/java/com/example/stockmarketdowjones/exception/DowJonesDataControllerAdvice.java
@@ -1,62 +1,50 @@
 package com.example.stockmarketdowjones.exception;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.http.HttpHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 
-@ControllerAdvice
-public class DowJonesDataControllerAdvice extends ResponseEntityExceptionHandler {
- 
-    @Autowired
-    private MessageSource messages;
- 
-    @ExceptionHandler({ FileNotFoundException.class })
-    public ResponseEntity<Object> handleFileNotFound(RuntimeException ex, WebRequest request) {
-        logger.error("404 Status Code", ex);
-        GenericResponse bodyOfResponse = new GenericResponse(
-		messages.getMessage("message.dowjonesdata.notfound", null, request.getLocale()), "404 Not Found");
-         
-        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+@RestControllerAdvice
+public class DowJonesDataControllerAdvice {
+    private static final Logger logger = LoggerFactory.getLogger(DowJonesDataControllerAdvice.class);
+	
+    @ExceptionHandler(StorageException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleStorage(StorageException exc) {
+        logger.error("400 Status Code", exc.getMessage());
+    	return exc.getMessage();
     }
- 
-    @ExceptionHandler({ StorageException.class })
-    public ResponseEntity<Object> handleStorage(RuntimeException ex, WebRequest request) {
-        logger.error("400 Status Code", ex);
-        GenericResponse bodyOfResponse = new GenericResponse(
-		messages.getMessage("message.file.storage.error", null, request.getLocale()), "400 Bad Request");
-        							
-        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-    }  
-
- 
-    @ExceptionHandler({ DataAlreadyExistsException.class })
-    public ResponseEntity<Object> handleDataAlreadyExists(RuntimeException ex, WebRequest request) {
-        logger.error("422 Status Code", ex);
-        GenericResponse bodyOfResponse = new GenericResponse(
-		messages.getMessage("message.data.alreadyexists", null, request.getLocale()), "422 Unprocessable Entity");
+    
+    @ExceptionHandler(FileNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleFileNotFound(FileNotFoundException exc) {
+        logger.error("404 Status Code", exc.getMessage());        
+    	return exc.getMessage();
+    }
+    
+    @ExceptionHandler(DataAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public String handleDataAlreadyExists(DataAlreadyExistsException exc) {
+        logger.error("422 Status Code", exc.getMessage());
          
-        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
+    	return exc.getMessage();
     }
     
 	@ExceptionHandler(MaxUploadSizeExceededException.class)
-	public ResponseEntity<Object> handleMaxSizeException(MaxUploadSizeExceededException ex, WebRequest request) {
-        logger.error("417 Status Code", ex);
-        GenericResponse bodyOfResponse = new GenericResponse(
-		messages.getMessage("message.file.toolarge", null, request.getLocale()), "417 Expectation Failed");
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+	public String handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        logger.error("417 Status Code", exc.getMessage());
 
-        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.EXPECTATION_FAILED, request);
+    	return exc.getMessage();
 	}
+    
+    
 }
-
-
 
 
 
